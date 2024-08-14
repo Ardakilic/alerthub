58e976befe7c6df39ce08c029674af1608c60b15
Silvio Cesar <silvio@silvio.cesar.nom.br>
2024-08-14 13:33:57 -0300

    fix indentation

--- src/utils/telegram.mjs
+++ src/utils/telegram.mjs
@@ -8,18 +8,18 @@ export default class telegramUtils {
   static async sendTelegramNotification(config, feedData) {
     // replace the value below with the Telegram token you receive from @BotFather
     const token = config.notifications.telegram.config.token;
-	
+
     // read the doc from https://github.com/yagop/node-telegram-bot-api to know how to catch the chatId
     const chatId = config.notifications.telegram.config.chatid;
-    
+
     const bot = new TelegramBot(token, { polling: false });
-    
-	try {
-	    let details = { 
-		  link: `<a target="_blank" href="${feedData.link}">${feedData.title}</a>`,
-		  description: feedData.description
-	    };
-		
+
+    try {
+        let details = {
+          link: `<a target="_blank" href="${feedData.link}">${feedData.title}</a>`,
+          description: feedData.description
+        };
+
         bot.sendMessage(chatId, `${feedData.link}\n${striptags(feedData.description)}` + '\n\n<pre>' + JSON.stringify(details, null, 2) + '</pre>', {
           parse_mode: 'html'
         });
